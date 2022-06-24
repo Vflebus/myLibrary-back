bee3ac6a8ba1a95c19794ccb480183e51bc58f5e
Vincent Flebus <vflebus@gmail.com>
2022-06-24 14:43:46 +0200

    delete route

--- router.js
+++ router.js
@@ -13,31 +13,52 @@ router.get('/', async (req, res) => {
 })
 
 //Avoir 1 livre
-router.get('/:id', async (req, res) => {
-    res.send('Avoir 1 livre')
+router.get('/:id', getBook, async (req, res) => {
+    res.json(res.book)
 })
 
 //Créer 1 livre
 router.post('/', async (req, res) => {
     try {
         const book = new Book({
-            title: req.body.title,
-            note: req.body.note,
+            ...req.body
         })
         const newBook = await book.save();
         res.status(201).json(newBook);
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
+})
+
+//Modifier 1 livre
+router.patch('/:id', getBook, (req, res) => {
+    res.send('Modifier 1 livre')
+})
+
+//Supprimer 1 livre
+router.delete('/:id', getBook, (req, res) => {
+    try {
+        res.book.remove()
+        res.json({
+            message: 'Le livre a bien été supprimé.'
+        })
     } catch (err) {
         res.status(500).json({message: err.message});
     }
 })
 
-//Modifier 1 livre
-router.get('/:id', (req, res) => {
-    res.send('Modifier 1 livre')
-})
-
-//Supprimer 1 livre
-router.delete('/:id', (req, res) => {
-    res.send('Supprimer 1 livre')
-})
+async function getBook(req, res, next) {
+    try{
+        let book = await Book.findById(req.params.id);
+        if (book == null){
+            return res.status(404).json({
+                message: 'Livre non trouvé.'
+            })
+        }
+        res.book = book;
+        next()
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+}
 module.exports = router
